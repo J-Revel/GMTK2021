a14d891ee44145213b7ea0f02ad36ad913f95a19
J-Revel <j.revel@protonmail.com>
2021-06-13 12:35:24 +0200

    Added bin destruction

--- Project/Assets/Scripts/Pushable.cs
+++ Project/Assets/Scripts/Pushable.cs
@@ -7,17 +7,18 @@ public class Pushable : MonoBehaviour
     public GameObject toHide;
     public GameObject toShow;
     public Transform[] fx;
+    public Transform fxSpawnPoint;
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.GetComponent<Character>() != null)
         {
             toHide.SetActive(false);
             toShow.SetActive(true);
+            for(int i=0; i<fx.Length; i++)
+            {
+                Instantiate(fx[i], fxSpawnPoint.position, Quaternion.identity);
+            }
+            Destroy(gameObject);
         }
-        for(int i=0; i<fx.Length; i++)
-        {
-            Instantiate(fx[i], transform.position, Quaternion.identity);
-        }
-        Destroy(gameObject);
     }
 }
